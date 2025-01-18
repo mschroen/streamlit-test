592ebd822e34f78f0e024b5b3fb749406901badf
Martin Schrön <post@mschroen.de>
2025-01-18 03:23:47 +0000

    dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
+from figurex import Figure
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
