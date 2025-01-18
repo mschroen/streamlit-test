41fe2721015ba052dec42d69ba97e6273e61686f
Martin Schrön <post@mschroen.de>
2025-01-18 03:51:01 +0000

    neptoon1

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,6 +87,37 @@ with Figure() as ax:
     ax.plot([1,2,3], [4,5,6])
 st.pyplot(Figure.get())
 
+# import pandas as pd
+from pathlib import Path
+from neptoon.workflow.process_with_yaml import ProcessWithYaml
+from neptoon.config import ConfigurationManager
+from neptoon.data_audit import DataAuditLog
+
+
+config = ConfigurationManager()
+
+station_config_path = Path(__file__).parent/'data/A101_station.yaml'
+processing_config_path = Path(__file__).parent/'data/v1_processing_method.yaml'
+
+config.load_and_validate_configuration(
+    name="station",
+    file_path=station_config_path,
+)
+config.load_and_validate_configuration(
+    name="processing",
+    file_path=processing_config_path,
+)
+
+DataAuditLog.create()
+yaml_processor = ProcessWithYaml(configuration_object=config)
+
+## OPTION 1:
+# data_hub = yaml_processor.create_data_hub()
+
+## OPTION 2:
+yaml_processor.run_full_process()
+
+
 # Add some spacing
 ''
 ''
